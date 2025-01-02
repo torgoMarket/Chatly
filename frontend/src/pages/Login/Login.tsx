348f9bec02c80c6b0116c0c144058361c1498b58
Amir <amirYuld@gmail.com>
2025-01-02 12:19:23 +0500

    [ADD] RegisterForm

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { Form } from '../../components/Form/Form'
 import { Field } from '../../components/Layouts/Field/Field'
 import { Button } from '../../components/UI/Button/Button'
@@ -9,6 +10,8 @@ export const Login = () => {
 				<Field label='email'></Field>
 				<Field label='password'></Field>
 				<Button>Login</Button>
+				<Link to='/register'>No Account?</Link>
+				<Link to='/recover'>Forget Password?</Link>
 			</Form>
 		</div>
 	)
--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom'
+import { Form } from '../../components/Form/Form'
+import { Field } from '../../components/Layouts/Field/Field'
+import { Button } from '../../components/UI/Button/Button'
+import styles from './Register.module.scss'
 export const Register = () => {
-	return <div>Register</div>
+	return (
+		<div className={styles.register}>
+			<Form>
+				<Field label='email'></Field>
+				<Field label='password'></Field>
+				<Field label='repeat password'></Field>
+				<Button>Register</Button>
+				<Link to='/login'>Have Account?</Link>
+			</Form>
+		</div>
+	)
 }
