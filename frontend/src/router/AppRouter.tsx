6bd9b68cff7bb75b0dc6afa7c9797eb11d3cc2fe
Amir <amirYuld@gmail.com>
2025-01-02 12:25:48 +0500

    [ADD] RecoverForm

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import { Home } from '../pages/Home/Home'
 import { Login } from '../pages/Login/Login'
+import { Recover } from '../pages/Recover/Recover'
 import { Register } from '../pages/Register/Register'
 
 const AppRouter: React.FC = () => {
@@ -11,6 +12,7 @@ const AppRouter: React.FC = () => {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/login' element={<Login />} />
+				<Route path='/recover' element={<Recover />} />
 				<Route path='/register' element={<Register />} />
 			</Routes>
 		</Router>
